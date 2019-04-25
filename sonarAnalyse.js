4778ebb169cfb28bfe91a2c2316c22516fe4287f
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-04-25 19:25:34 +0200

    Sonar debug

--- sonarAnalyse.js
+++ sonarAnalyse.js
@@ -6,7 +6,7 @@ scanner(
             'sonar.projectKey': 'svalinn-discord-bot',
             'sonar.sources': '.',
             'sonar.login': 'a0bc4034df242d95ec5af92f2ddc6a68d8db8cbf',
-            'sonar.projectVersion': '1.0',
+            'sonar.projectVersion': '2.0',
             'sonar.sourceEncoding': 'UTF-8',
             'sonar.typescript.tslint.reportPaths': 'report.json',
             'sonar.exclusions': 'sonarAnalyse.js',
