3e69a92d9eca4449fb97054ab2715b497b6c2521
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-04-25 19:53:55 +0200

    operation interface

---
+++ lib/interfaces/operation.ts
@@ -0,0 +1,29 @@
+import { Moment } from 'moment';
+
+/**
+ * Interface of the operation
+ */
+export interface IOperation {
+
+    // ID of the event
+    ID: number;
+
+    // ID of the server
+    serverID: string;
+
+    // Name of the operation
+    name: string;
+
+    // Description of the operation
+    description: string;
+
+    // Creator ID
+    creatorID: string;
+
+    // date of the event
+    date: Moment;
+
+    // Map avec la l'ID de l'utilisateur et son pseudo
+    participants: Map<string, string>;
+
+}
