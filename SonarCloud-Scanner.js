9ea5accb9cf94916cb884c78a796c71449c68ed2
Thibault SOUQUET <contact@thibaultsouquet.fr>
2019-10-15 16:26:35 +0200

    GitHub actions (#9)
    * Initial Github actions setup
    
    * Github actions files
    
    * Github actions debug uri
    
    * Github actions debug
    
    * Github actions remove database

---
+++ SonarCloud-Scanner.js
@@ -0,0 +1,20 @@
+const scanner = require('sonarqube-scanner');
+const nodePackage = require('./package.json');
+scanner(
+    {
+        options: {
+            'sonar.host.url': 'https://sonarcloud.io',
+            'sonar.projectKey': 'Falcort_discord-bot-event',
+            'sonar.organization': 'tsouquet-back',
+            'sonar.projectVersion': nodePackage.version,
+            'sonar.sources': '.',
+            'sonar.login': process.env.SONAR_TOKEN,
+            'sonar.projectName': 'Discord Bot Event',
+            'sonar.sourceEncoding': 'UTF-8',
+            'sonar.typescript.tslint.reportPaths': 'report.json',
+            'sonar.exclusions': 'SonarCloud-Scanner.js',
+        }
+    }, () => {
+        // Callback is required
+    }
+);
