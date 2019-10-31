f13cdee261f2b4610aeb6039428805e6012d153f
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-10-31 13:50:38 +0100

    Coverage : utils

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -14,4 +14,11 @@ describe('Utils', () => {
         process.env.GH_ACTIONS = 'true';
         expect(getMongoDbConnectionString()).contain('mongodb://localhost:27017/');
     });
+
+    it('getMongoDbConnectionString() : Not Github Actions environment - should be ok', () => {
+        const before = process.env.GH_ACTIONS;
+        process.env.GH_ACTIONS = null;
+        expect(getMongoDbConnectionString()).to.match(/^(mongodb:\/\/.{1,}:.{1,}@.{1,}:.{1,}\/.{1,})/);
+        process.env.GH_ACTIONS = before;
+    });
 });
