fd5d76328ddb24e99a638292a0ef8e104965cdad
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-11-02 12:36:59 +0100

    Code reformat

--- lib/models/operation.ts
+++ lib/models/operation.ts
@@ -7,30 +7,30 @@ import { IOperation } from '../interfaces/operation';
  */
 const OperationSchema: Schema = new Schema({
 
-  serverID: { // ID of the server
-    type: String,
-    required: true
-  },
-  name: { // Name of the operation
-    type: String,
-    required: true
-  },
-  description: { // Description of the operation
-    type: String,
-    required: true
-  },
-  creatorID: { // Creator ID
-    type: String,
-    required: true
-  },
-  date: { // date of the event
-    type: Number,
-    required: true
-  },
-  participants: { // Map avec la l'ID de l'utilisateur et son pseudo
-    type: Map,
-    required: true
-  }
+    serverID: { // ID of the server
+        type: String,
+        required: true
+    },
+    name: { // Name of the operation
+        type: String,
+        required: true
+    },
+    description: { // Description of the operation
+        type: String,
+        required: true
+    },
+    creatorID: { // Creator ID
+        type: String,
+        required: true
+    },
+    date: { // date of the event
+        type: Number,
+        required: true
+    },
+    participants: { // Map avec la l'ID de l'utilisateur et son pseudo
+        type: Map,
+        required: true
+    }
 });
 
 export default mongoose.model<IOperation>('Operations', OperationSchema);
