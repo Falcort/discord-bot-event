c5672bb4a13039c661c13127bc3f42caeeee1fee
Dermi <dermi@laposte.net>
2019-11-03 21:24:37 +0100

    Coverage: logger => logFromLevel()

--- tests/logger.spec.ts
+++ tests/logger.spec.ts
@@ -3,27 +3,27 @@ import 'mocha';
 import { Logger } from '../lib/class/logger';
 
 describe('Logger', () => {
-    it('trace', () => {
+    it('lofFromLevel: Should return trace', () => {
         expect(new Logger().logFromLevel('trace', 'test')).contain('trace');
     });
 
-    it('info', () => {
+    it('lofFromLevel: Should return info', () => {
         expect(new Logger().logFromLevel('info', 'test')).contain('info');
     });
 
-    it('warn', () => {
+    it('lofFromLevel: Should return warn', () => {
         expect(new Logger().logFromLevel('warn', 'test')).contain('warn');
     });
 
-    it('error', () => {
+    it('lofFromLevel: Should return error', () => {
         expect(new Logger().logFromLevel('error', 'test')).contain('error');
     });
 
-    it('fatal', () => {
+    it('lofFromLevel: Should return fatal', () => {
         expect(new Logger().logFromLevel('fatal', 'test')).contain('fatal');
     });
 
-    it('default', () => {
+    it('lofFromLevel: Should return default', () => {
         expect(new Logger().logFromLevel('default', 'test')).contain('default');
     });
 });
