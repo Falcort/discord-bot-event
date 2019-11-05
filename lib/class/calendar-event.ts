b5f069c10ca082e01e75f507a992b2bf0b4a2cb7
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-11-05 14:04:28 +0100

    Log refactor

--- lib/class/calendar-event.ts
+++ lib/class/calendar-event.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 import { IConfig } from '../interfaces/config';
+import { ILog } from '../interfaces/log';
 import { IOperation } from '../interfaces/operation';
 import OperationModel from '../models/operation';
 import operation from '../models/operation';
@@ -14,11 +15,13 @@ class CalendarEvent {
      * @param eventID -- The ID of the event the user want to join
      * @param username -- Username
      * @param userID -- UserId of the user that want to join the event
-     * @param command -- the used command
+     * @param partialLog -- the partial log to complete
      * @return string -- The result message of the function
      */
-    public static async addParticipant(eventID: string, username: string, userID: string, command: string) {
+    public static async addParticipant(eventID: string, username: string, userID: string, partialLog: ILog) {
 
+        partialLog.function = 'addParticipant()';
+        partialLog.eventID = eventID;
         return await OperationModel.findOne({_id: eventID}).then(
             async (success: IOperation) => {
 
@@ -30,7 +33,8 @@ class CalendarEvent {
 
                         if (key === username || value === userID) {
                             response = `<@${userID}> tu participes déjà à l'opération : ${success.name}`;
-                            logger.logAndDB(command, userID, 'info', response);
+                            partialLog.result = response;
+                            logger.logAndDB(partialLog);
                         }
 
                     });
@@ -41,10 +45,13 @@ class CalendarEvent {
                         response = OperationModel.updateOne({_id: eventID}, {$set: {participants: success.participants}}).then(
                             () => {
                                 const successMessage = `<@${userID}> merci pour ta participation à l'opération : ${operation.name} le ${DateTime.fromMillis(success.date).setLocale('fr').toLocaleString(DateTime.DATETIME_SHORT)}`;
-                                logger.logAndDB(command, userID, 'info', successMessage);
+                                partialLog.result = successMessage;
+                                logger.logAndDB(partialLog);
                                 return successMessage;
                             }, error => {
-                                logger.logAndDB(command, userID, 'error', error);
+                                partialLog.level = 'error';
+                                partialLog.result = error;
+                                logger.logAndDB(partialLog);
                                 return 'Erreur inconnu';
                             }
                         );
@@ -52,43 +59,65 @@ class CalendarEvent {
                     return response;
                 }
                 const errorMessage = `Aucune opération ne porte l\'id : ${eventID}`;
-                logger.logAndDB(command, userID, 'info', errorMessage);
+                partialLog.result = errorMessage;
+                partialLog.level = 'warn';
+                logger.logAndDB(partialLog);
                 return errorMessage;
             }, error => {
-                logger.logAndDB(command, userID, 'error', error);
+                partialLog.level = 'error';
+                partialLog.result = error;
+                logger.logAndDB(partialLog);
                 return 'Erreur inconnu';
             }
         );
     }
 
-    public static async deleteOperation(operationID: string, userID: string, command: string) {
-        return await OperationModel.findOne({_id: operationID}).then(
+    /**
+     * This function is to delete an event
+     * Only admins and event creator can remove an event
+     *
+     * @param eventID -- The ID of the event to delete
+     * @param userID -- The ID of the user issuing the command
+     * @param partialLog -- the partial log to complete
+     */
+    public static async deleteOperation(eventID: string, userID: string, partialLog: ILog) {
+        partialLog.function = 'deleteOperation()';
+        partialLog.eventID = eventID;
+        return await OperationModel.findOne({_id: eventID}).then(
             async (success: IOperation) => {
 
                 if (success) {
 
                     if (success.creatorID === userID || config.admins.includes(userID)) {
-                        return await OperationModel.deleteOne({_id: operationID}).then(
+                        return await OperationModel.deleteOne({_id: eventID}).then(
                             () => {
-                                const successMessage = `L'Opération : ${operationID} a bien été supprimée`;
-                                logger.logAndDB(command, userID, 'info', successMessage);
+                                const successMessage = `L'Opération : ${eventID} a bien été supprimée`;
+                                partialLog.result = successMessage;
+                                logger.logAndDB(partialLog);
                                 return successMessage;
                             }, error => {
-                                logger.logAndDB(command, userID, 'error', error);
+                                partialLog.level = 'error';
+                                partialLog.result = error;
+                                logger.logAndDB(partialLog);
                                 return 'Erreur inconnu';
                             }
                         );
                     }
                     const permissionMessage = `Seul le créateur d'une opération peut la supprimer`;
-                    logger.logAndDB(command, userID, 'info', permissionMessage);
+                    partialLog.result = permissionMessage;
+                    logger.logAndDB(partialLog);
                     return permissionMessage;
                 }
-                const errorMessage = `L'Opération avec l'ID : ${operationID}, n'existe pas`;
-                logger.logAndDB(command, userID, 'info', errorMessage);
+                const errorMessage = `L'Opération avec l'ID : ${eventID}, n'existe pas`;
+                partialLog.result = errorMessage;
+                partialLog.level = 'warn';
+                logger.logAndDB(partialLog);
                 return errorMessage;
 
             }, error => {
-                logger.logAndDB(command, userID, 'error', error);
+                partialLog.level = 'error';
+                partialLog.result = error;
+                logger.logAndDB(partialLog);
                 return 'Erreur inconnu';
             }
         );
@@ -99,10 +128,13 @@ class CalendarEvent {
      * @param username -- Username of the user that want to leave
      * @param userID -- UserID of the user that want to leavse
      * @param eventID -- ID of the event the user want to leave
-     * @param command -- the executed command
+     * @param partialLog -- the partial log to complete
      * @return string -- The result messages of the function
      */
-    public static async removeParticipant(username: string, userID: string, eventID: string, command: string) {
+    public static async removeParticipant(username: string, userID: string, eventID: string, partialLog: ILog) {
+
+        partialLog.function = 'removeParticipant()';
+        partialLog.eventID = eventID;
 
         return await OperationModel.findOne({_id: eventID}).then(
             async (success: IOperation) => {
@@ -121,16 +153,20 @@ class CalendarEvent {
 
                     if (!response) {
                         response = `<@${userID}> tu ne participes pas à l'opération : ${success.name} du ${DateTime.fromMillis(success.date).setLocale('fr').toLocaleString(DateTime.DATETIME_SHORT)}`;
-                        logger.logAndDB(command, userID, 'info', response);
+                        partialLog.result = response;
+                        logger.logAndDB(partialLog);
                     } else {
 
                         response = await OperationModel.updateOne({_id: eventID}, {$set: {participants: success.participants}}).then(
                             () => {
                                 const successMessage = `<@${userID}> tu ne participes plus à l'opération : ${success.name} du ${DateTime.fromMillis(success.date).setLocale('fr').toLocaleString(DateTime.DATETIME_SHORT)}`;
-                                logger.logAndDB(command, userID, 'info', successMessage);
+                                partialLog.result = successMessage;
+                                logger.logAndDB(partialLog);
                                 return successMessage;
                             }, error => {
-                                logger.logAndDB(command, userID, 'error', error);
+                                partialLog.result = error;
+                                partialLog.level = 'error';
+                                logger.logAndDB(partialLog);
                                 return 'Erreur inconnu';
                             }
                         );
@@ -140,10 +176,14 @@ class CalendarEvent {
                     return response;
                 }
                 const errorMessage = `Aucune opération ne porte l\'id : ${eventID}`;
-                logger.logAndDB(command, userID, 'info', errorMessage);
+                partialLog.result = errorMessage;
+                partialLog.level = 'warn';
+                logger.logAndDB(partialLog);
                 return errorMessage;
             }, error => {
-                logger.logAndDB(command, userID, 'error', error);
+                partialLog.level = 'error';
+                partialLog.result = error;
+                logger.logAndDB(partialLog);
                 return 'Erreur inconnu';
             }
         );
@@ -156,9 +196,9 @@ class CalendarEvent {
      * @param name -- Name of the event
      * @param description -- Description of the event
      * @param serverID -- The serverID, used to know which event is on which server
-     * @param username -- The usernme of the user
+     * @param username -- The username of the user
      * @param userID -- The userID of the user
-     * @param command -- the executed command
+     * @param partialLog -- the partial log to complete
      * @return string -- The error/success message to display
      */
     public static async validateAndCreatOperation(date: string,
@@ -168,7 +208,9 @@ class CalendarEvent {
                                                   serverID: string,
                                                   username: string,
                                                   userID: string,
-                                                  command: string) {
+                                                  partialLog: ILog) {
+
+        partialLog.function = 'validateAndCreatOperation()';
 
         if (date !== undefined &&
             time !== undefined &&
@@ -183,7 +225,11 @@ class CalendarEvent {
                 const luxon = DateTime.fromFormat(`${date} ${time}`, 'dd/MM/yyyy HH:mm').setLocale('fr').toMillis();
                 const current = DateTime.local().setLocale('fr').toMillis();
                 if (luxon <= current) {
-                    return `L'opération ne peut pas étre dans le passé`;
+                    const errorMessage = `L'opération ne peut pas étre dans le passé`;
+                    partialLog.level = 'warn';
+                    partialLog.result = errorMessage;
+                    logger.logAndDB(partialLog);
+                    return errorMessage;
                 }
 
                 const participants = new Map<string, string>().set(username, userID);
@@ -200,10 +246,14 @@ class CalendarEvent {
                 return await new OperationModel(operationToCreate).save().then(
                     (success: IOperation) => {
                         const returnMessage = `Opération (ID: ${success.id}) créée avec succès, merci de ta participation <@${userID}> !`;
-                        logger.logAndDB(command, userID, 'info', returnMessage);
+                        partialLog.result = returnMessage;
+                        partialLog.eventID = success.id.toString();
+                        logger.logAndDB(partialLog);
                         return returnMessage;
                     }, error => {
-                        logger.logAndDB(command, userID, 'error', error);
+                        partialLog.level = 'error';
+                        partialLog.result = error;
+                        logger.logAndDB(partialLog);
                         return 'Erreur inconnu';
                     }
                 );
@@ -211,7 +261,8 @@ class CalendarEvent {
 
         }
         const message = `Erreur : commande incorrecte...`;
-        logger.logAndDB(command, userID, 'warn', message);
+        partialLog.level = 'warn';
+        partialLog.result = message;
         return message;
     }
 
@@ -220,7 +271,8 @@ class CalendarEvent {
      *
      * @return string -- The list of all existing event
      */
-    public static async listAllEvents(command: string, userID: string) {
+    public static async listAllEvents(command: string, partialLog: ILog) {
+        partialLog.function = 'listAllEvents()';
         return await OperationModel.find({date: {$gt: DateTime.local().setLocale('fr').toMillis()}}).then(
             (success: IOperation[]) => {
                 let message = `Voici la liste des opérations en cours :\n\n`;
@@ -234,21 +286,36 @@ class CalendarEvent {
                     });
                     message += `\n`;
                 }
-                logger.logAndDB(command, userID, 'info', message);
+
+                partialLog.result = message;
+                logger.logAndDB(partialLog);
                 return message;
             }, error => {
-                logger.logAndDB(command, userID, 'error', error);
+                partialLog.result = error;
+                partialLog.level = 'error';
+                logger.logAndDB(partialLog);
                 return 'Erreur inconnu';
             }
         );
     }
 
-    public static async getAllEventFromDate(date: DateTime) {
-        return await OperationModel.find({date: {$gt: DateTime.local().setLocale('fr').toMillis()}}).then(
+    /**
+     * This function return all event from the given date
+     *
+     * @param date -- Datetime -- the date to search from
+     * @return Promise<number | IOperation[]> -- Number if error, else array of events
+     */
+    public static async getAllEventFromDate(date: DateTime): Promise<number | IOperation[]> {
+        return await OperationModel.find({date: {$gt: date.toMillis()}}).then(
             (success: IOperation[]) => {
                 return success;
             }, error => {
-                logger.logAndDB('getAllEventFromDate()', 'inetnal-request', 'error', error);
+                logger.logAndDB({
+                    command: 'getAllEventFromDate()',
+                    function: 'getAllEventFromDate()',
+                    result: error,
+                    level: 'error'
+                } as ILog);
                 return -1;
             }
         );
