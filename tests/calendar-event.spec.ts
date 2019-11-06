0bc867619bf6027d085c42872c95090bc3d65ef7
Dermi <dermi@laposte.net>
2019-11-06 23:11:44 +0100

    Coverage: validateAndCreateOperation() => sucess

--- tests/calendar-event.spec.ts
+++ tests/calendar-event.spec.ts
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import 'mocha';
 import CalendarEvent from '../lib/class/calendar-event';
 import { IConfig } from '../lib/interfaces/config';
+import Operation from '../lib/models/operation';
 import { getMongoDbConnectionString } from '../lib/utils/functions';
+
 const config: IConfig = require('../config.json');
 import * as mongoose from 'mongoose';
 
@@ -30,6 +32,19 @@ describe('Calendar event', () => {
         const command = `${config.config.prefix}listOpé`;
         const message = await CalendarEvent.listAllEvents(command, '127085518579040257');
         expect(message).contain('Voici la liste des opérations en cours :');
+    });
+
+    it('validateAndCreateOperation(): Should return succes', async () => {
+        const message = await CalendarEvent.validateAndCreatOperation('22/03/2031',
+            '21:00',
+            'The league of explorers',
+            'Explore the galaxy',
+            '1',
+            'Bender',
+            '1',
+            'ok');
+        const operationID = message.match(/[a-z0-9]{24}/);
+        expect(message).contain(`Opération (ID: ${operationID}) créée avec succès, merci de ta participation <@1> !`);
     });
 
 });
