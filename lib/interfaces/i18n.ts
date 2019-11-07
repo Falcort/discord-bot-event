fa4be0f50b89889406c3f900feda6ad4b2f61e54
SOUQUET Thibault <contact@thibaultsouquet.fr>
2019-11-07 10:26:28 +0100

    [i81n] : fr-FR - help

---
+++ lib/interfaces/i18n.ts
@@ -0,0 +1,3 @@
+export interface II18n {
+    'help': string;
+}
--- lib/utils/functions.ts
+++ lib/utils/functions.ts
@@ -5,19 +5,6 @@ import { IConfig } from '../interfaces/config';
 const config: IConfig = require('../../config.json');
 
 /**
- * Function that send all commands to the user
- * @return string -- The message to send with all commandes details
- */
-export function help(): string {
-    let response = '';
-    response += '**Créer une opération** : --addOpé DD/MM/YYYY HH:mm Nom Description\n';
-    response += '**Lister les opérations** : --listOpé\n';
-    response += '**Rejoindre une opération** : --joinOpé ID\n';
-    response += '**Quitter une opération** : --leaveOpé ID';
-    return response;
-}
-
-/**
  * Function that clean the channel
  * This function call itslef while there is still message to delete
  * @param Bot -- The Bot variable
